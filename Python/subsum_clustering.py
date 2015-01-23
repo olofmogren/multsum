2ec48d1cbbf38f635cb71947b54edb7464ac769e
Olof Mogren <mogren@chalmers.se>
2015-01-23 15:53:46 +0100

    Licensing information.

--- Python/subsum_clustering.py
+++ Python/subsum_clustering.py
@@ -5,11 +5,28 @@ import math, numpy, os.path, re, sys,time
 from subprocess import call
 
  # @author Olof Mogren
+ #
+ # email: olof.mogren@gmail.com
+ #
  # 
  # Base class for submodular summarization.
  # 
  # Providing some base functions.
  #
+ # Released under the GNU GPL.
+ # 
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 CLUTO_SCLUSTER_EXECUTABLE = "/home/mogren/btsync/code/others_code/cluto/scluster"
